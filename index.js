3e31785100bd2c8350af670a4b3ac849f856ed18
SherleyZhang <sherley@SherleydeMacBook-Pro.local>
2021-12-08 10:12:37 -0800

    first

--- index.js
+++ index.js
@@ -70,14 +70,7 @@ function createClient() {
   return client;
 }
 
-if (process.env.NODE_ENV === "production"{
-      app.use(express.static("build"));
-      app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname,  "build", "index.html"));
-      });
-}
-    
-app.listen(process.env.PORT || 3000); //http://localhost:3000/api/posts
+app.listen(3000); //http://localhost:3000/api/posts
 
 //git init
 //git status
