4313438bb1d40eeea5ba02d87686916d5024ec0f
SherleyZhang <sherley@SherleydeMacBook-Pro.local>
2021-12-08 08:51:28 -0800

    Add existing file

---
+++ index.js
@@ -0,0 +1,61 @@
+require("dotenv").config();
+
+// npm run dev
+const express = require("express"); //CommonJS
+const pg = require("pg");
+const cors = require("cors");
+const Client = pg.Client;
+const app = express();
+app.use(express.json()); //middleware to deserialize JSON in request
+app.use(cors()); //middleare to allow cross-domain AJAX requiests
+
+app.get("/api/comments", (request, response) => {
+  const client = createClient();
+  client.connect().then(() => {
+    client.query("select * from comments").then((queryResponse) => {
+      response.json(queryResponse.rows);
+    });
+  });
+  //response.json([1,2]);
+});
+
+app.get("/api/comments/:id", (request, response) => {
+  const client = createClient();
+  client.connect().then(() => {
+    client
+      .query("select * from posts where id=$1", [request.params.id])
+      .then((queryResponse) => {
+        response.json(queryResponse.rows[0]);
+      });
+  });
+});
+
+// post request: get request是refresh就自然发生，但是post request需要手动（比如postman上）
+app.post("/api/posts", (request, response) => {
+  const client = createClient();
+  client.connect().then(() => {
+    client
+      .query(
+        "insert into comments (names, posts) values ('sherley', 'love it!!!') returning *",
+        [request.body.title, request.body.body]
+      )
+      .then((queryResponse) => {
+        response.json(queryResponse.rows[0]);
+      });
+  });
+});
+
+function createClient() {
+  const client = new Client({
+    connectionString: process.env.CONNECTION_STRING,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+  return client;
+}
+
+app.listen(3000); //http://localhost:3000/api/posts
+
+//git init
+//git status
